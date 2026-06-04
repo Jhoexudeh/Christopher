93a1cad4c7ca164904771368dc196e8fe2124a39
Joe <115124780+Jhoexudeh@users.noreply.github.com>
2026-06-04 15:11:19 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -38,7 +38,6 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-    console.log("Google Response:", data);
 
     if (!response.ok) {
       return res.status(response.status).json(data);
